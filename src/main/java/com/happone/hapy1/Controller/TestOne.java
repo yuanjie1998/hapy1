340ee090b2873b08d7330525a804f773f6b70514
yuanjie <1916819402@qq.com>
2021-03-08 09:32:02 +0800

    testContent

--- src/main/java/com/happone/hapy1/Controller/TestOne.java
+++ src/main/java/com/happone/hapy1/Controller/TestOne.java
@@ -5,4 +5,7 @@ package com.happone.hapy1.Controller;
  * @Date 2021/3/8 9:29
  */
 public class TestOne {
+    public static void main(String[] args) {
+        System.out.println("testOne");
+    }
 }
