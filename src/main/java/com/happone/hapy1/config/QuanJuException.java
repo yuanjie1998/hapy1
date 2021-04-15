1956bf3a89bd22b6b29876676e4a5be1dbd443ef
yuanjie <1916819402@qq.com>
2021-04-15 12:22:55 +0800

    new 1

---
+++ src/main/java/com/happone/hapy1/config/QuanJuException.java
@@ -0,0 +1,16 @@
+package com.happone.hapy1.config;
+
+import com.happone.hapy1.Controller.TestOne;
+import com.happone.hapy1.Controller.Yuanjie;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author yuan jie
+ * @Date 2021/4/15 12:20
+ */
+@Configuration
+public class QuanJuException {
+    public static void main(String[] args) {
+        System.out.println("...全局异常的异常类");
+    }
+}
